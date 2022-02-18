61a625ee293b17eb16c86b142155a1e04e95c6d2
Sweekrithi123 <99883562+Sweekrithi123@users.noreply.github.com>
2022-02-18 23:11:03 +0530

    Add files via upload

---
+++ mail1.py
@@ -0,0 +1,10 @@
+input1=str(input("input a string:"))
+for i in range(0,4):
+    input2=str(input("enter a names:"))
+    for j in str(i):
+        mail=input1+" "+input2+"\n"
+    print(mail)
+
+
+
+ 
---
+++ mail3.py
@@ -0,0 +1,10 @@
+input1="Hello Freind"
+for i in range(0,4):
+    input2=str(input("enter a names:"))
+    for j in str(i):
+        mail=input1.replace("Freind",input2)+"\n"
+    print(mail)
+
+
+
+ 
